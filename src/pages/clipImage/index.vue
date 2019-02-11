f22ca86eeeb6bb793b97e4a942b16c9aaefa7a9f
pc <575720013@qq.com>
2019-02-11 21:39:58 +0800

    `

--- src/pages/clipImage/index.vue
+++ src/pages/clipImage/index.vue
@@ -2,14 +2,14 @@
     <div>
         <div>原图：</div>
         <section class="sec sec-origin">
-            <image class="image" src="https://resources.laihua.com/2019-1-26/6f5e4ad0-2133-11e9-97a0-49d3e303954e.png" mode="aspectFit"></image>
+            <image class="image" src="https://resources.laihua.com/2019-1-5/6e5631c0-10ad-11e9-853b-0beb71690b72.png" mode="aspectFit"></image>
         </section>
         <div>裁切后：</div>
         <section class="sec sec-cliped">
             <image class="image" :src="clipedUrl" mode="aspectFit"></image>
         </section>
         <!-- 封面裁切组件 -->
-        <clip-image :url="'https://resources.laihua.com/2019-1-26/6f5e4ad0-2133-11e9-97a0-49d3e303954e.png'"
+        <clip-image :url="'https://resources.laihua.com/2019-1-5/6e5631c0-10ad-11e9-853b-0beb71690b72.png'"
             :ratio="[5,4]"
             @clipOver="clipOverHdl" @clipImageHide="clipHideHdl"></clip-image>
     </div>
